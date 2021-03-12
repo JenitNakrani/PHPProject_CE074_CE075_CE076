9cb6c399262c9d95a01a6fa34512d0021f60059a
JenitNakrani <jenitnakrani123@gmail.com>
2021-03-12 15:58:30 +0530

    abc

---
+++ PHPProject_CE074_CE075_CE076/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!-- app/views/user/show.blade.php -->
+@extends('layouts.master')
+
+@section('content')
+<div class="panel panel-default">
+    <div class="panel-heading"><label>View User</label>
+        <a class ='pull-right' href="{{ Request::header('referer') }}">
+            <i class="glyphicon glyphicon-circle-arrow-left"></i> Go Back
+        </a>
+    </div>
