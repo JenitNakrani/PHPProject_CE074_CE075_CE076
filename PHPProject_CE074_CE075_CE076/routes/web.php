57797cab4ef6e553f88da08d584f757687add1c8
JenitNakrani <jenitnakrani123@gmail.com>
2021-03-12 18:15:13 +0530

    error fixed

--- PHPProject_CE074_CE075_CE076/routes/web.php
+++ PHPProject_CE074_CE075_CE076/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('welcome/', function () {
     return view('welcome');
 });
 
@@ -21,7 +21,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::get('home/', function () {
+Route::get('/', function () {
     return view('home');
 });
 
