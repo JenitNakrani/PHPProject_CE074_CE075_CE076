62f9694b24cdf01472558e107a9f40d3852abfaa
JenitNakrani <jenitnakrani123@gmail.com>
2021-03-15 20:00:45 +0530

    error fixed in model

--- PHPProject_CE074_CE075_CE076/app/Models/Book.php
+++ PHPProject_CE074_CE075_CE076/app/Models/Book.php
@@ -8,6 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
-
     public $timestamps = FALSE;
 }
--- PHPProject_CE074_CE075_CE076/database/migrations/2021_03_15_141926_create_books_table.php
+++ PHPProject_CE074_CE075_CE076/database/migrations/2021_03_15_141926_create_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBookTable extends Migration
+class CreateBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateBookTable extends Migration
      */
     public function up()
     {
-        Schema::table('book', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('book_name');
             $table->string('author_name');
             $table->char('publish_year', 4);  
             $table->boolean('is_available');
+
         });
     }
 
@@ -29,8 +30,6 @@ class CreateBookTable extends Migration
      */
     public function down()
     {
-        Schema::table('book', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('books');
     }
 }
